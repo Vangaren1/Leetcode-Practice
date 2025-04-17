2d5a55ab5a1c61e707663f5c033b0f232d505bf4
MarkThompson5318 <mthompson@steamshipauthority.com>
2025-04-17 16:50:54 -0400

    merge two sorted list complete.

--- Grind75/week1/merge_two_sorted_lists/Solution.java
+++ Grind75/week1/merge_two_sorted_lists/Solution.java
@@ -3,21 +3,26 @@ import common.ListNode;
 
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode ptr1 = list1;
-        ListNode ptr2 = list2;
-        ListNode tmp;
-        while(ptr1 != null && ptr2 != null){
-            if(ptr1.val <= ptr2.val){
-                tmp = ptr2.next;
-                ptr2.next = ptr1;
-                ptr2 = tmp;
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                tail.next = list1;
+                list1 = list1.next;
             }else{
-                ptr1 = ptr1.next;
+                tail.next = list2;
+                list2 = list2.next;
             }
+            tail = tail.next;
         }
-        
+        if(list1 != null){
+            tail.next = list1;
+        }
+        else{
+            tail.next = list2;
+        }
 
-        return list1;
+        return head.next;
     }
     
 
--- common/listnode.py
+++ common/listnode.py
@@ -4,4 +4,16 @@ class ListNode:
         self.val = val
         self.next = next
         
-        
+def build_list(values):
+    head = ListNode(0)
+    curr = head
+    for val in values:
+        curr.next = ListNode(val)
+        curr = curr.next
+    return head.next
+
+def print_list(node):
+    while node:
+        print(node.val, end=" -> ")
+        node = node.next
+    print("None")
