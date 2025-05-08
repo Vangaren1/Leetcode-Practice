2a2b8944fdc6275ae67073065650f4460bf86755
Vangaren1 <markthompson6807@gmail.com>
2025-05-07 22:47:21 -0400

    clone graph complete

--- Grind75/week3/clone_graph_133/Solution.java
+++ Grind75/week3/clone_graph_133/Solution.java
@@ -1,7 +1,52 @@
 package Grind75.week3.clone_graph_133;
 
+import common.Node;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
+    public Node cloneGraph(Node node) {
+        bfs(node);
+        return seen.get(node);
+    }
+
+    public Map<Node, Node> seen = new HashMap<>();
+
+    public void bfs(Node node){
+        if(!seen.containsKey(node) && node != null){
+            seen.put(node, new Node(node.val));
+            for(Node tmp: node.neighbors){
+                if(!seen.containsKey(tmp)){
+                    bfs(tmp);
+                }
+                Node newTmp = seen.get(tmp);
+                seen.get(node).neighbors.add(newTmp);
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+
+
+        a.neighbors.add(b);
+        a.neighbors.add(d);
+        b.neighbors.add(a);
+        b.neighbors.add(c);
+        c.neighbors.add(b);
+        c.neighbors.add(d);
+        d.neighbors.add(a);
+        d.neighbors.add(c);
+
+        Solution s = new Solution();
+
+        Node newNode = s.cloneGraph(a);
+
+
+
         System.out.println("Running clone_graph_133...");
     }
 }
--- Grind75/week3/clone_graph_133/clone_graph_133.py
+++ Grind75/week3/clone_graph_133/clone_graph_133.py
@@ -4,7 +4,24 @@ from common.graphnode import GraphNode as Node, print_graph_bfs, build_graph
 
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        pass
+        seen = {}
+
+        def bfs(n: Optional['Node']):
+            nonlocal seen
+            if n in seen:
+                return n
+            seen[n] = Node(n.val)
+            for neigh in n.neighbors:
+                if neigh in seen:
+                    seen[n].neighbors.append(seen[neigh])
+                else:
+                    bfs(neigh)
+                    seen[n].neighbors.append(seen[neigh])
+                
+        bfs(node)
+        return seen[node]
+        
+
 
 if __name__ == "__main__":
     print("Running Solution...")
