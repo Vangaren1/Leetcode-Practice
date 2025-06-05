af83402d8ed07463f977dc5bc57aefd75fbcfb02
MarkThompson5318 <mthompson@steamshipauthority.com>
2025-06-05 09:37:51 -0400

    acct merge

--- Grind75/week5/account_merge_721/Solution.java
+++ Grind75/week5/account_merge_721/Solution.java
@@ -1,7 +1,111 @@
 package Grind75.week5.account_merge_721;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class Solution {
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        
+        QuickUnion qu = new QuickUnion(accounts.size());
+
+        Map<String, Integer> emailToAccount = new HashMap<>();
+
+        for ( int index = 0; index < accounts.size(); index ++  ){
+            List<String> acct = accounts.get(index);
+            for ( int j = 1; j < acct.size(); j++){
+                if ( emailToAccount.containsKey( acct.get(j))){
+                    qu.union( index , emailToAccount.get(acct.get(j)));
+                }else{
+                    emailToAccount.put( acct.get(j), index);
+                }
+            }
+        }
+
+        Map<Integer, List<String>> newAcct = new HashMap<>();
+
+        int leadAcct = 0;
+        for (Map.Entry<String, Integer> entry : emailToAccount.entrySet()) {
+            String email = entry.getKey();
+            Integer index = entry.getValue();
+
+            leadAcct = qu.root(index);
+            
+            boolean keyExists = newAcct.containsKey(leadAcct);
+
+            if ( keyExists ){
+                newAcct.get(leadAcct).add(email);
+            }else{
+                List<String> curr = new ArrayList<>();
+                curr.add(email);
+                newAcct.put(leadAcct, curr);
+            }
+        }
+
+        List<List<String>> result = new ArrayList<>();
+
+        for ( int acctnum : newAcct.keySet()){
+            String name = accounts.get(acctnum).get(0);
+            List<String> emails = newAcct.get(acctnum);
+            Collections.sort(emails);
+            emails.add(0, name);
+            result.add(emails);
+        }
+
+        return result;
+
+    }
+
+    class QuickUnion{
+
+        int[] id;
+
+        public QuickUnion(int size){
+            id = new int[size];
+            for ( int i = 0; i < size; i++){
+                id[i] = i;
+            }
+        }
+
+        public int root(int index){
+            while (index != id[index]){
+                index = id[index];
+            }
+            return index;
+        }
+
+        public boolean connected(int p, int q){
+            return root(p) == root(q);
+        }
+
+        public void union(int p, int q){
+            int rootp = root(p);
+            int rootq = root(q);
+            id[rootp] = rootq;
+        }
+    }
+
     public static void main(String[] args) {
+
+        List<List<String>> data = Arrays.asList(
+            Arrays.asList("David", "David0@m.co", "David0@m.co", "David1@m.co", "David2@m.co", "David3@m.co", "David4@m.co", "David5@m.co", "David6@m.co", "David7@m.co"),
+            Arrays.asList("David", "David0@m.co", "David0@m.co", "David1@m.co", "David2@m.co", "David3@m.co", "David4@m.co", "David5@m.co", "David6@m.co", "David7@m.co"),
+            Arrays.asList("David", "David2@m.co", "David18@m.co", "David19@m.co", "David20@m.co", "David21@m.co", "David22@m.co", "David23@m.co", "David24@m.co", "David25@m.co"),
+            Arrays.asList("David", "David3@m.co", "David27@m.co", "David28@m.co", "David29@m.co", "David30@m.co", "David31@m.co", "David32@m.co", "David33@m.co", "David34@m.co"),
+            Arrays.asList("David", "David1@m.co", "David9@m.co", "David10@m.co", "David11@m.co", "David12@m.co", "David13@m.co", "David14@m.co", "David15@m.co", "David16@m.co")
+        );
+
+        Solution s = new Solution();
+
+        List<List<String>> result = s.accountsMerge(data);
+
+        for ( List<String> tmp : result){
+            System.out.println(tmp);
+        }
         System.out.println("Running account_merge_721...");
     }
 }
--- Grind75/week5/account_merge_721/account_merge_721.py
+++ Grind75/week5/account_merge_721/account_merge_721.py
@@ -1,9 +1,55 @@
 from typing import Optional, List
+from collections import defaultdict
+class QuickUnion:
+    
+    def __init__(self, size):
+        self.id = [ i for i in range(size) ]
+        
+    def root(self, index):
+        while index != self.id[index]:
+            self.id[index] = self.id[self.id[index]] 
+            index = self.id[index]
+        return index
+
+    def connected(self, p, q):
+        return self.root(p) == self.root(q)
+    
+    def union(self, p, q):
+        rootP = self.root(p)
+        rootQ = self.root(q)
+        self.id[rootP] = rootQ
 
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        pass
+        
+        qu = QuickUnion(len(accounts))
+        
+        emailToAccount = {}
+        
+        for index, acct in enumerate(accounts):
+            for email in acct[1:]:
+                if email in emailToAccount:
+                    qu.union(index, emailToAccount[email])
+                else:
+                    emailToAccount[email] = index 
+        
+        newAcct = defaultdict( list )
+        
+        for email, index in emailToAccount.items():
+            leadAcct = qu.root(index)
+            newAcct[leadAcct].append(email)
+            
+        result = []
+        
+        for index in newAcct.keys():
+            name = accounts[index][0]
+            e = newAcct.get(index)
+            e.sort()
+            e.insert(0,name)
+            result.append( e )
+        
+        return result
 
 if __name__ == "__main__":
     accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
