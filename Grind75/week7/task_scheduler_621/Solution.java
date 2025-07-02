0d19f14020045be5ef743dcb491bed9bee75fe7b
Vangaren1 <markthompson6807@gmail.com>
2025-07-01 22:13:09 -0400

    task scheduler java finished

--- Grind75/week7/task_scheduler_621/Solution.java
+++ Grind75/week7/task_scheduler_621/Solution.java
@@ -1,26 +1,60 @@
 package Grind75.week7.task_scheduler_621;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 class Solution {
+    class Pair{
+        int count, time;
+        public Pair(int count, int time){
+            this.count = count;
+            this.time = time;
+        }
+    }
     public int leastInterval(char[] tasks, int n) {
 
         Map<Character, Integer> taskDict = new HashMap<>();
         for (int index = 0; index < tasks.length; index++ ) {
             taskDict.merge(tasks[index], 1,Integer::sum);
         }
-        Map<Character, Integer> waitQueue = new HashMap<>();
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+        for ( int c : taskDict.values()) {
+            maxHeap.add(-c);
+        }
 
-        int count = 0;
+        Deque<Pair> qu = new ArrayDeque<>();
 
-        while (!taskDict.isEmpty()) {
-            count++;
+        int time = 0;
+
+        while (!maxHeap.isEmpty() || !qu.isEmpty()) {
+            time++;
+            if ( !maxHeap.isEmpty()) {
+                int cnt = 1 + maxHeap.poll();
+                if ( cnt < 0){
+                    qu.add(new Pair(cnt, time + n));
+                }
+            }
+            if ( !qu.isEmpty() && qu.peek().time == time) {
+                maxHeap.add( qu.pollFirst().count );
+            }
             
         }
-        return 0;
+        return time;
     }
     public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        String test = "AAABBB";
+
+        System.out.println(s.leastInterval(test.toCharArray(), 2));
+
+
+
         System.out.println("Running task_scheduler_621...");
     }
 }
--- Grind75/week7/task_scheduler_621/task_scheduler_621.py
+++ Grind75/week7/task_scheduler_621/task_scheduler_621.py
@@ -1,46 +1,32 @@
 from typing import Optional, List
 
-from collections import defaultdict
+from collections import defaultdict, Counter, deque
+import heapq
+
+
 
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        taskDict = defaultdict(int)
-        for t in tasks:
-            taskDict[t] += 1
-        waitQueue = defaultdict(lambda: n)
+        count = Counter(tasks)
+        maxheap = [ -c for c in count.values()]
+        heapq.heapify(maxheap) 
+        q = deque()
+        time = 0
+        while maxheap or q:
+            time += 1
+            if maxheap:
+                cnt = 1 + heapq.heappop(maxheap)
+                if cnt:
+                    q.append((cnt, time + n))
+            if q and q[0][1] == time:
+                heapq.heappush(maxheap, q.popleft()[0])
+        return time
         
         
-        
-        count = 0
-        while taskDict.keys():
-            
-            count += 1
-            
-            curr = None
-            potential = [ (key, val) for key, val in taskDict.items() if key not in waitQueue.keys()]
-            potential.sort(key= lambda x: x[1], reverse=True)
-            if potential:
-                curr = potential[0][0]
-                taskDict[curr] -= 1
-                if taskDict[curr] == 0:
-                    taskDict.pop(curr)
-            
-            if curr:
-                waitQueue[curr] = n+1
-            
-            #reduce the countdown for each item in the wait Queue
-            #remove if countdown is 0
-            keyQueue = list(waitQueue.keys())
-            for key in keyQueue:
-                waitQueue[key] -= 1
-                if waitQueue[key] == 0:
-                    waitQueue.pop(key)
-                
-        return count
 
 if __name__ == "__main__":
-    tasks = ["A","A","A","B","B","B", "C","C","C", "D", "D", "E"]
+    tasks = ["A","A","A","B","B","B"]
     n =2
     
     s = Solution()
