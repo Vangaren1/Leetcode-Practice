5d403d6a3efeff27b5812860882dd8d23b02c59d
Vangaren1 <markthompson6807@gmail.com>
2025-07-06 19:20:49 -0400

    minimum height tree complete.

--- Grind75/week7/minimum_height_tree_310/Solution.java
+++ Grind75/week7/minimum_height_tree_310/Solution.java
@@ -1,7 +1,71 @@
 package Grind75.week7.minimum_height_tree_310;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> retVal = new ArrayList<>();
+        if (n==1) {
+            retVal.add(0);
+            return retVal;
+        }
+        Map<Integer,List<Integer>> adj = new HashMap<>();
+        for ( int[] edge : edges) {
+            if( adj.containsKey(edge[0])){
+                adj.get(edge[0]).add(edge[1]);
+            } else{
+                adj.put(edge[0], new ArrayList<>());
+                adj.get(edge[0]).add(edge[1]);
+            }
+            if( adj.containsKey(edge[1])){
+                adj.get(edge[1]).add(edge[0]);
+            } else{
+                adj.put(edge[1], new ArrayList<>());
+                adj.get(edge[1]).add(edge[0]);
+            }
+        }
+
+        Map<Integer, Integer> edgeCount = new HashMap<>();
+        Deque<Integer> leaf = new ArrayDeque<>();
+        for ( int src : adj.keySet()) {
+            List<Integer> tmp = adj.get(src);
+            if ( tmp.size() == 1) {
+                leaf.add(src);
+            }
+            edgeCount.put(src, tmp.size());
+        }
+
+        while ( !leaf.isEmpty()) {
+            if ( n <= 2){
+                break;
+            }
+            int leafSize = leaf.size();
+            for ( int index = 0; index < leafSize; index ++){
+                int node = leaf.removeFirst();
+                n--;
+                for ( int nei : adj.get(node)) {
+                    edgeCount.put(nei, edgeCount.get(nei) - 1);
+                    if ( edgeCount.get(nei) == 1) leaf.add(nei);
+                }
+            }
+        }
+
+        return new ArrayList<>(leaf);
+    }
+
     public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int n = 4;
+        int edges[][] = {{1,0},{1,2},{1,3}};
+
+        System.out.println(s.findMinHeightTrees(n, edges));
         System.out.println("Running minimum_height_tree_310...");
     }
 }
--- Grind75/week7/minimum_height_tree_310/minimum_height_tree_310.py
+++ Grind75/week7/minimum_height_tree_310/minimum_height_tree_310.py
@@ -1,50 +1,42 @@
 from typing import Optional, List
-from collections import deque
+from collections import deque, defaultdict
+
+
 
 class Solution:
-    class Union:
-        def __init__(self, size):
-            self.id = list(range(size))
-            self.size = [1] * size
-        def root(self, i):
-            while i != self.id[i]:
-                self.id[i] = self.id[self.id[i]] 
-                i = self.id[i]
-            return i
-        def connected(self, p, q):
-            return self.root(p) == self.root(q)
-        def union(self, p, q):
-            i = self.root(p)
-            j = self.root(q)
-            if i != j:
-                if self.size[i] < self.size[j]:
-                    self.id[i] = j
-                    self.size[j] += self.size[i]
-                else:
-                    self.id[j] = i
-                    self.size[i] += self.size[j]
-            
-            
-            
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        for index in range(len(edges)):
-            edgeSet = deque(edges)
-            edgeSet.rotate(index)
-            un = self.Union(n)
-            print('new order')
-            for left, right in list(edgeSet):
-                print("{} {}".format(left, right))
-                un.union(left, right)
-            print(list(set(un.id)))
-        return list(set(un.id))
+        if n == 1:
+            return [0]
+        adj = defaultdict(list)
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
         
-
+        edgeCount = {}
+        leaf = deque()
+        for src, neighbor in adj.items():
+            if len(neighbor) == 1:
+                leaf.append(src)
+            edgeCount[src] = len(neighbor)
+            
+        while leaf:
+            if n <= 2:
+                return list(leaf)
+            for index in range(len(leaf)):
+                node = leaf.popleft()
+                n -= 1
+                for nei in adj[node]:
+                    edgeCount[nei] -= 1
+                    if edgeCount[nei] == 1:
+                        leaf.append(nei)
+            
+        
+    
 if __name__ == "__main__":
     n = 6
     edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
     
     s = Solution()
-    u = s.Union(n)
 
     print(s.findMinHeightTrees(n, edges))
     
