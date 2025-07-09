942d1494a96ab1ea30fd07dadfd3e9a2530a9331
Mark Thompson <mthompson@steamshipauthority.com>
2025-07-09 14:07:26 -0400

    2 java solutions added

--- Extra/summary_ranges_228/Solution.java
+++ Extra/summary_ranges_228/Solution.java
@@ -1,7 +1,55 @@
 package Extra.summary_ranges_228;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> retVal = new ArrayList<>();
+        
+        if ( nums.length == 0) return retVal;
+        
+        if ( nums.length == 1) { 
+            retVal.add( Integer.toString(nums[0]));
+            return retVal;
+        }
+
+        int ptr1 = 0;
+        int ptr2 = 0;
+
+        while ( ptr1 < nums.length) {
+
+            while ( ptr2 < nums.length - 1) {
+                int diff = Math.abs(nums[ptr2+1] - nums[ptr2]);
+                if (diff == Integer.MIN_VALUE) break;
+                if ( diff > 1){
+                    break;
+                }
+                ptr2++;
+            }
+            
+            retVal.add(strNum(nums[ptr1],nums[ptr2]));
+
+            ptr1 = ptr2 + 1;
+            ptr2++;
+
+        }
+        return retVal;
+
+    }
+
+    public String strNum(int a, int b) {
+        if ( a == b) {
+            return Integer.toString(a);
+        }
+        return Integer.toString(a) + "->" + Integer.toString(b);
+    }
+
     public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {-2147483648,0,2,3,4,6,8,9};
+
+        System.out.println(sol.summaryRanges(nums));
         System.out.println("Running summary_ranges_228...");
     }
 }
--- Grind75/week8/minimum_window_substring/Solution.java
+++ Grind75/week8/minimum_window_substring/Solution.java
@@ -2,55 +2,24 @@ package Grind75.week8.minimum_window_substring;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 class Solution {
-    public List<String> summaryRanges(int[] nums) {
-        List<String> retVal = new ArrayList<>();
+    public String minWindow(String s, String t) {
         
-        if ( nums.length == 0) return retVal;
-        
-        if ( nums.length == 1) { 
-            retVal.add( Integer.toString(nums[0]));
-            return retVal;
-        }
-
-        int ptr1 = 0;
-        int ptr2 = 0;
-
-        while ( ptr1 < nums.length) {
-
-            while ( ptr2 < nums.length - 1) {
-                int diff = Math.abs(nums[ptr2+1] - nums[ptr2]);
-                if (diff == Integer.MIN_VALUE) break;
-                if ( diff > 1){
-                    break;
-                }
-                ptr2++;
-            }
-            
-            retVal.add(strNum(nums[ptr1],nums[ptr2]));
-
-            ptr1 = ptr2 + 1;
-            ptr2++;
-
-        }
-        return retVal;
-
     }
-
-    public String strNum(int a, int b) {
-        if ( a == b) {
-            return Integer.toString(a);
+    
+    public boolean validSub(Map<Character,Integer> a, Map<Character,Integer> b) {
+        if ( b.keySet().size() > a.keySet().size()) return false;
+        for ( char key : b.keySet()) {
+            if ( b.get(key) > a.get(key)) return false;
         }
-        return Integer.toString(a) + "->" + Integer.toString(b);
+        return true;
     }
-
     public static void main(String[] args) {
-        Solution sol = new Solution();
+    Solution sol = new Solution();    
 
-        int[] nums = {-2147483648,0,2,3,4,6,8,9};
 
-        System.out.println(sol.summaryRanges(nums));
         System.out.println("Running minimum_window_substring...");
     }
 }
