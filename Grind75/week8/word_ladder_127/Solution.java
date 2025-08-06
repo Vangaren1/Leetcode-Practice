b572fe1b7513c555756112d6bb1331c7107d9d52
Vangaren1 <markthompson6807@gmail.com>
2025-08-05 21:47:10 -0400

    work home update

---
+++ Grind75/week8/word_ladder_127/Solution.java
@@ -0,0 +1,7 @@
+package Grind75.week8.word_ladder_127;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println("Running word_ladder_127...");
+    }
+}
---
+++ Grind75/week8/word_ladder_127/word_ladder_127.py
@@ -0,0 +1,47 @@
+from typing import Optional, List
+
+import string
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordSet = set(wordList)
+
+        queue = [(beginWord, 1)]
+
+        if endWord not in wordSet:
+            return 0
+
+        if len(endWord) != len(beginWord):
+            return 0
+
+        if beginWord == endWord:
+            return 0
+
+        letters = string.ascii_lowercase
+
+        while queue:
+
+            curr, step = queue.pop(0)
+
+            for index in range(len(curr)):
+                for l in letters:
+                    tmp = curr[:index] + l + curr[index + 1 :]
+                    if tmp in wordSet:
+                        if tmp == endWord:
+                            return step + 1
+                        queue.append((tmp, step + 1))
+                        wordSet.remove(tmp)
+
+        return 0
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    beginWord = "hit"
+    endWord = "cog"
+    wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
+
+    print(sol.ladderLength(beginWord, endWord, wordList))
+    print("Running Solution...")
