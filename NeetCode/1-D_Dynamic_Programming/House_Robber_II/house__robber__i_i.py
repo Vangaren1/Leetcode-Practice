cd5f7177b023b8822f2bf9fe6a2dd2b97198c370
Vangaren1 <markthompson6807@gmail.com>
2025-09-09 09:51:03 -0400

    dynamic programming 2

--- NeetCode/1-D_Dynamic_Programming/House_Robber_II/house__robber__i_i.py
+++ NeetCode/1-D_Dynamic_Programming/House_Robber_II/house__robber__i_i.py
@@ -4,6 +4,12 @@ from typing import Optional, List
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
+        first = nums[0] + self.rob1(nums[2:-1])
+        second = self.rob1(nums[1:])
+        return max(first, second)
+
+    def rob1(self, nums: List[int]) -> int:
+
         rob1, rob2 = 0, 0
 
         for n in nums:
@@ -15,5 +21,8 @@ class Solution:
 
 if __name__ == "__main__":
     sol = Solution()
-
+    nums = [1, 2, 3, 1]
+    print(sol.rob(nums))
+    nums = [2, 9, 8, 3, 6]
+    print(sol.rob(nums))
     print("Running Solution...")
