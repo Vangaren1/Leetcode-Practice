6af0c0d2f412a94e33537dc7cf326b29eb87bc5e
Mark Thompson <mthompson@steamshipauthority.com>
2026-03-05 09:26:02 -0500

    container with most water c++

--- NeetCode/Two_Pointers/Container_with_most_water/Container_with_most_water.cpp
+++ NeetCode/Two_Pointers/Container_with_most_water/Container_with_most_water.cpp
@@ -21,7 +21,30 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    // TODO: paste the LeetCode method signature here.
+    int maxArea(vector<int> &heights)
+    {
+        int left{0}, maxSeen{0}, curr{0}, lVal{0}, rVal{0};
+        int right = heights.size() - 1;
+
+        while (left < right)
+        {
+            lVal = heights[left];
+            rVal = heights[right];
+            curr = (right - left) * min(lVal, rVal);
+            maxSeen = max(maxSeen, curr);
+
+            if (lVal <= rVal)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return maxSeen;
+    }
 };
