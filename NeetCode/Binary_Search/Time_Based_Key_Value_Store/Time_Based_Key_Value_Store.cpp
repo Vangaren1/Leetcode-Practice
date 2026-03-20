d7e7a289505941dc56933f881dbfbc09e1cc89bc
Vangaren1 <markthompson6807@gmail.com>
2026-03-19 22:00:33 -0400

    more binary in c++

--- NeetCode/Binary_Search/Time_Based_Key_Value_Store/Time_Based_Key_Value_Store.cpp
+++ NeetCode/Binary_Search/Time_Based_Key_Value_Store/Time_Based_Key_Value_Store.cpp
@@ -21,7 +21,46 @@
 
 using namespace std;
 
-class Solution {
+class TimeMap
+{
+private:
+    unordered_map<string, vector<pair<string, int>>> keymap;
+    string binarySearch(vector<pair<string, int>> &pairs, int timestamp)
+    {
+        int left{0}, mid{-1}, tmp{-1};
+        int right = pairs.size() - 1;
+
+        string result = "";
+        while (left <= right)
+        {
+            mid = left + (right - left) / 2;
+
+            if (pairs[mid].second <= timestamp)
+            {
+                result = pairs[mid].first;
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
 public:
-    // TODO: paste the LeetCode method signature here.
+    TimeMap()
+    {
+    }
+
+    void set(string key, string value, int timestamp)
+    {
+        keymap[key].push_back({value, timestamp});
+    }
+
+    string get(string key, int timestamp)
+    {
+        return binarySearch(keymap[key], timestamp);
+    }
 };
