d45ef17a16a9926b16f08529e39635cb3bed3f5a
Vangaren1 <markthompson6807@gmail.com>
2026-03-28 11:33:40 -0400

    trie in c++

--- NeetCode/Trie/Implement_Trie_Prefix_Tree/Implement_Trie_Prefix_Tree.cpp
+++ NeetCode/Trie/Implement_Trie_Prefix_Tree/Implement_Trie_Prefix_Tree.cpp
@@ -21,7 +21,62 @@
 
 using namespace std;
 
-class Solution {
+class PrefixTree
+{
+private:
+    class Node
+    {
+    public:
+        array<Node *, 26> children{};
+        bool terminal = false;
+    };
+    Node *root;
+
 public:
-    // TODO: paste the LeetCode method signature here.
+    PrefixTree()
+    {
+        root = new Node();
+    }
+
+    void insert(string word)
+    {
+        Node *ptr = root;
+        for (char ch : word)
+        {
+            if (ptr->children[ch - 'a'] == nullptr)
+            {
+                ptr->children[ch - 'a'] = new Node();
+            }
+            ptr = ptr->children[ch - 'a'];
+        }
+        ptr->terminal = true;
+    }
+
+    bool search(string word)
+    {
+        Node *ptr = root;
+        for (char ch : word)
+        {
+            if (ptr->children[ch - 'a'] == nullptr)
+            {
+                return false;
+            }
+            ptr = ptr->children[ch - 'a'];
+        }
+        return ptr->terminal;
+    }
+
+    bool startsWith(string prefix)
+    {
+        Node *ptr = root;
+        for (char ch : prefix)
+        {
+            if (ptr->children[ch - 'a'] == nullptr)
+            {
+                return false;
+            }
+            ptr = ptr->children[ch - 'a'];
+        }
+        return true;
+    }
 };
