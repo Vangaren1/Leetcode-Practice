3dafbd9cff900222cb35dd36eed9aef42bd8da25
Mark Thompson <mthompson@steamshipauthority.com>
2026-04-13 11:00:17 -0400

    more c++ neetcode

--- NeetCode/1-D_Dynamic_Programming/Min_Cost_Climbing_Stairs/Min_Cost_Climbing_Stairs.cpp
+++ NeetCode/1-D_Dynamic_Programming/Min_Cost_Climbing_Stairs/Min_Cost_Climbing_Stairs.cpp
@@ -21,7 +21,20 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    // TODO: paste the LeetCode method signature here.
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        vector<int> paid(cost);
+
+        paid.push_back(0);
+
+        for (int index = paid.size() - 3; index > -1; index--)
+        {
+            paid[index] = paid[index] + min(paid[index + 1], paid[index + 2]);
+        }
+
+        return min(paid[0], paid[1]);
+    }
 };
