407b26f85a72de4302d7e0657765073da012a00d
Mark Thompson <mthompson@steamshipauthority.com>
2026-06-01 15:41:33 -0400

    neetcode c++ detect squares

--- NeetCode/Math_And_Geometry/Multiply/Multiply.cpp
+++ NeetCode/Math_And_Geometry/Multiply/Multiply.cpp
@@ -21,7 +21,38 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    // TODO: paste the LeetCode method signature here.
+    string multiply(string num1, string num2)
+    {
+        long long one = convert(num1);
+        long long two = convert(num2);
+        long long product = one * two;
+
+        string result = "";
+        if (product == 0)
+        {
+            return "0";
+        }
+        while (product)
+        {
+            result = result + (char)(product % 10 + '0');
+            product = product / 10;
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+    long long convert(string num)
+    {
+        long long total = 0;
+        int tens = 1;
+        for (int ptr = num.size() - 1; ptr >= 0; ptr--)
+        {
+            int digit = (char)num[ptr] - '0';
+            total = total + (digit * tens);
+            tens = tens * 10;
+        }
+        return total;
+    }
 };
--- NeetCode/Math_And_Geometry/Set_Matrix_Zeros/Set_Matrix_Zeros.cpp
+++ NeetCode/Math_And_Geometry/Set_Matrix_Zeros/Set_Matrix_Zeros.cpp
@@ -21,7 +21,34 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    // TODO: paste the LeetCode method signature here.
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        vector<pair<int, int>> found;
+
+        for (int y = 0; y < matrix.size(); y++)
+        {
+            for (int x = 0; x < matrix[0].size(); x++)
+            {
+                if (matrix[y][x] == 0)
+                {
+                    found.push_back({y, x});
+                }
+            }
+        }
+        for (pair<int, int> coord : found)
+        {
+            func(matrix, coord.first, coord.second);
+        }
+    }
+    void func(vector<vector<int>> &matrix, int row, int col)
+    {
+        matrix[row] = vector<int>(matrix[0].size(), 0);
+        for (int r = 0; r < matrix.size(); r++)
+        {
+            matrix[r][col] = 0;
+        }
+    }
 };
