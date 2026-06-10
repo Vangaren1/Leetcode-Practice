5d296f09ceff7a371726558c781301c2f4476c37
Mark Thompson <mthompson@steamshipauthority.com>
2026-06-10 10:14:09 -0400

    neetcode burst balloon C++

--- NeetCode/2-D_Dynamic_Programming/Edit_Distance/Edit_Distance.cpp
+++ NeetCode/2-D_Dynamic_Programming/Edit_Distance/Edit_Distance.cpp
@@ -23,33 +23,54 @@ using namespace std;
 
 class Solution
 {
+    struct PairHash
+    {
+        size_t operator()(const pair<int, int> &p) const
+        {
+            size_t h1 = hash<int>{}(p.first);
+            size_t h2 = hash<int>{}(p.second);
+
+            return h1 ^ (h2 << 1);
+        }
+    };
+
 public:
     int minDistance(string word1, string word2)
     {
-        return cost(word1, word2, word1.size(), word2.size());
+        unordered_map<pair<int, int>, int, PairHash> dp;
+        if (word1 == word2)
+        {
+            return 0;
+        }
+        return recursive(0, 0, word1, word2, dp);
     }
-
-    int cost(const string &w1, const string &w2, int m, int n)
+    int recursive(int index1, int index2, string &word1, string &word2, unordered_map<pair<int, int>, int, PairHash> &dp)
     {
-        if (m == 0)
+        if (dp.count({index1, index2}))
         {
-            return n;
+            return dp[{index1, index2}];
+        }
+        if (index1 == word1.size())
+        {
+            dp[{index1, index2}] = word2.size() - index2;
+            return dp[{index1, index2}];
+        }
+        if (index2 == word2.size())
+        {
+            dp[{index1, index2}] = word1.size() - index1;
+            return dp[{index1, index2}];
+        }
+        if (word1[index1] == word2[index2])
+        {
+            dp[{index1, index2}] = recursive(index1 + 1, index2 + 1, word1, word2, dp);
+            return dp[{index1, index2}];
         }
 
-        if (n == 0)
-        {
-            return m;
-        }
-
-        if (w1[m - 1] == w2[n - 1])
-        {
-            return cost(w1, w2, m - 1, n - 1);
-        }
-
-        int subCost = 1 + cost(w1, w2, m - 1, n - 1);
-        int delCost = 1 + cost(w1, w2, m - 1, n);
-        int insCost = 1 + cost(w1, w2, m, n - 1);
-        return min(subCost, delCost, insCost);
+        int insert1 = 1 + recursive(index1 + 1, index2, word1, word2, dp);
+        int delete1 = 1 + recursive(index1, index2 + 1, word1, word2, dp);
+        int swap = 1 + recursive(index1 + 1, index2 + 1, word1, word2, dp);
+        dp[{index1, index2}] = min({insert1, delete1, swap});
+        return dp[{index1, index2}];
     }
 };
 
