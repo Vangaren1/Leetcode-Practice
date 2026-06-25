fed3324f181b1c60a8e3a3207dbc1a66f3ab3ad7
Mark Thompson <mthompson@steamshipauthority.com>
2026-06-25 12:54:47 -0400

    more c++ neetcode

--- NeetCode/Heap/Median_Finder/Median_Finder.cpp
+++ NeetCode/Heap/Median_Finder/Median_Finder.cpp
@@ -21,7 +21,57 @@
 
 using namespace std;
 
-class Solution {
+class MedianFinder
+{
+private:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int count;
+
 public:
-    // TODO: paste the LeetCode method signature here.
+    MedianFinder()
+    {
+        count = 0;
+    }
+
+    void addNum(int num)
+    {
+        count++;
+        if (maxHeap.empty())
+        {
+            maxHeap.push(num);
+        }
+        else
+        {
+            if (maxHeap.top() > num)
+            {
+                maxHeap.push(num);
+            }
+            else
+            {
+                minHeap.push(num);
+            }
+        }
+
+        if (maxHeap.size() > minHeap.size() + 1)
+        {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+
+        if (maxHeap.size() < minHeap.size())
+        {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    double findMedian()
+    {
+        if (count % 2 == 1)
+        {
+            return maxHeap.top();
+        }
+        return (maxHeap.top() / 2.0) + (minHeap.top() / 2.0);
+    }
 };
