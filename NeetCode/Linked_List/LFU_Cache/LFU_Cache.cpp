c9d556b5a7e2256cd29107368fb9dc428d82c441
Mark Thompson <mthompson@steamshipauthority.com>
2026-08-03 14:03:42 -0400

    added c++ for LFU

--- NeetCode/Linked_List/LFU_Cache/LFU_Cache.cpp
+++ NeetCode/Linked_List/LFU_Cache/LFU_Cache.cpp
@@ -37,29 +37,65 @@ private:
     unordered_map<int, int> count;
     unordered_map<int, list<int>> freq;
     unordered_map<int, list<int>::iterator> keyIter;
-    int least = 0;
+    int least;
     int cap;
 
 public:
     LFUCache(int capacity)
     {
         cap = capacity;
+        least = 0;
     }
 
     int get(int key)
     {
+        if (!keyVal.count(key))
+        {
+            return -1;
+        }
+        _increaseFreq(key);
+        return keyVal[key];
     }
 
     void put(int key, int value)
     {
+        if (cap == 0)
+        {
+            return;
+        }
+        if (keyVal.count(key))
+        {
+            _increaseFreq(key);
+            keyVal[key] = value;
+            return;
+        }
+        if (keyVal.size() == cap)
+        {
+            // erase the least frequently used.
+            int leastKey = freq[least].front();
+            freq[least].pop_front();
+            keyVal.erase(leastKey);
+            count.erase(leastKey);
+            keyIter.erase(leastKey);
+        }
+
+        keyVal[key] = value;
+        count[key] = 0;
+        keyIter[key] = freq[0].insert(freq[0].end(), key);
+        least = 0;
     }
 
     void _increaseFreq(int key)
     {
-        int f = count[key];
-        freq[f].erase(keyIter[key]);
-        freq[f + 1].push_back(key);
-        keyIter[key] = prev(freq[f + 1].end());
+        int curr = count[key];
+        count[key]++;
+        freq[curr].erase(keyIter[key]);
+        freq[curr + 1].push_back(key);
+        keyIter[key] = prev(freq[curr + 1].end());
+        if (curr == least && freq[curr].empty())
+        {
+            least++;
+        }
     }
 };
 
